dd300aef8a06845eabb0d678e08f46b28fcfb356
zww <785495110@qq.com>
2024-02-18 23:22:16 +0800

    100

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,38 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <h2 class="my-title">vue3学习记录</h2>
+      </el-header>
+      <el-container>
+        <el-aside width="300px" height="100%">
+          <el-scrollbar>
+            <el-menu :default-active="activeIndex" :router="true">
+              <el-sub-menu index="/">
+                <template #title>
+                  <el-icon>
+                    <message />
+                  </el-icon>父子组件通信
+                </template>
+                <el-menu-item index="/about">v-model</el-menu-item>
+                <el-menu-item index="/">主页</el-menu-item>
+              </el-sub-menu>
+            </el-menu>
+          </el-scrollbar></el-aside>
+        <el-main>
+          <RouterView></RouterView>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
+<script setup>
+import { ref } from 'vue'
+const activeIndex = ref("/")
+</script>
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+.my-title {
   text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+//引入饿了么ui
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +13,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+//使用饿了么ui
+app.use(ElementPlus)
 
 app.mount('#app')
